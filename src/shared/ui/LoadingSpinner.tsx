edb7b724aace4eba1350df19d819e83269f9d38c
MoonHongMyeong <moonhongmyeong@gmail.com>
2024-12-18 15:52:57 +0900

    Refactor: loading spinner 레이아웃 사이즈 변경 [#2]

--- src/shared/ui/LoadingSpinner.tsx
+++ src/shared/ui/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 export const LoadingSpinner = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center min-h-[calc(100vh-48px)] md:min-h-[calc(100vh-64px)]">
       <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
     </div>
   )
