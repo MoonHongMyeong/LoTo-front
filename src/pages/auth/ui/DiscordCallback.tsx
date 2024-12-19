ec6c8bac1339f94b0010203539a8398bcd596c8d
MoonHongMyeong <moonhongmyeong@gmail.com>
2024-12-19 15:51:10 +0900

    fix: Discord 로그인 중복 요청 방지[#3]
    - 개발 환경에서의 중복 API 호출 방지
    - React Strict Mode 대응

--- src/pages/auth/ui/DiscordCallback.tsx
+++ src/pages/auth/ui/DiscordCallback.tsx
@@ -1,11 +1,13 @@
-import { useEffect } from 'react'
-import { useNavigate, useSearchParams } from 'react-router-dom'
-import { discordApi } from '@/features/auth/api/discordApi'
 import { AuthResponse } from '@/entities/auth'
+import { discordApi } from '@/features/auth/api/discordApi'
+import { LoadingSpinner } from '@/shared/ui/LoadingSpinner'
+import { useEffect, useRef } from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 export const DiscordCallback = () => {
   const [searchParams] = useSearchParams()
   const navigate = useNavigate()
+  const isRequesting = useRef(false)
 
   useEffect(() => {
     const code = searchParams.get('code')
@@ -17,22 +19,25 @@ export const DiscordCallback = () => {
 
     const handleAuth = async () => {
       try {
+        console.log("isRequesting: ", isRequesting)
+        if (isRequesting.current) return
+        isRequesting.current = true
+
         const response: AuthResponse = await discordApi.login(code)
         
         localStorage.setItem('accessToken', response.accessToken)
-        navigate('/main')
+        navigate('/')
 
       } catch (error) {
         console.error('Discord auth failed:', error)
         navigate('/error')
+      } finally {
       }
     }
     handleAuth()
   }, [searchParams, navigate])
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="text-lg">로그인 처리중...</div>
-    </div>
+    <LoadingSpinner />
   )
 } 
