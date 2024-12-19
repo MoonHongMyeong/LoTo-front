5016dbf1697e0254fef04e63daa2cc0e12f3e13c
MoonHongMyeong <moonhongmyeong@gmail.com>
2024-12-19 13:50:46 +0900

    Refactor: 모든 에러에 에러페이지로 보내는 건 유저 경험에 안좋을 것 같아서 해당 부분 삭제 [#1]

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -5,7 +5,6 @@ import { Main } from '@/pages/main/ui/Main'
 import { Profile } from '@/pages/profile/ui/Profile'
 import { Shareroom } from '@/pages/shareroom/ui/Shareroom'
 import { Character } from '@/pages/character/ui/Character'
-import { Error404Page } from '@/pages/_error/ui/Error404Page'
 import { Layout } from '@/pages/_layout/ui/Layout'
 import { DiscordCallback } from '@/pages/auth/ui/DiscordCallback'
 import { LoadingSpinner } from '@/shared/ui/LoadingSpinner'
@@ -31,32 +30,26 @@ export const router = createBrowserRouter([
   {
     path: '/auth/discord/callback',
     element: <DiscordCallback />,
-    errorElement: <Error404Page />
   },
   {
     path: '/',
     element: <Layout><Login /></Layout>,
-    errorElement: <Error404Page />
   },
   {
     path: '/main',
     element: <ProtectedRoute><Layout><Main /></Layout></ProtectedRoute>,
-    errorElement: <Error404Page />
   },
   {
     path: '/@me',
     element: <ProtectedRoute><Layout><Profile /></Layout></ProtectedRoute>,
-    errorElement: <Error404Page />
   },
   {
     path: '/shareroom/:shareroomId',
     element: <ProtectedRoute><Layout><Shareroom /></Layout></ProtectedRoute>,
-    errorElement: <Error404Page />
   },
   {
     path: '/character',
     element: <ProtectedRoute><Layout><Character /></Layout></ProtectedRoute>,
-    errorElement: <Error404Page />
   },
   
 ])
